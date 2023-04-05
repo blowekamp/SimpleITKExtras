SimpleITK/SimpleITKUtilities

Add dask.from_sitk method to read images into dask

test/conftest.py
@@ -1,0 +1,69 @@
+import pytest
+import SimpleITK as sitk
+from pathlib import Path
+from itertools import product, chain
+
+_params = product(
+    [sitk.sitkInt8, sitk.sitkUInt8, sitk.sitkInt16, sitk.sitkUInt16, sitk.sitkFloat32],
+    ["nrrd", "nii"],
+)
+
+_params = chain(
+    _params,
+    product(
+        [
+            sitk.sitkInt8,
+            sitk.sitkInt16,
+            sitk.sitkInt16,
+            sitk.sitkInt32,
+            sitk.sitkFloat32,
+        ],
+        ["mrc"],
+    ),
+)
+
+
+def to_ids(p):
+    return f"{p[1]}_{sitk.GetPixelIDValueAsString([0])}"
+
+
+@pytest.fixture(scope="session", params=_params)
+def image_mha(request, tmp_path_factory) -> Path:
+    pixel_type, extension = request.param
+    fn = f"image_{sitk.GetPixelIDValueAsString(pixel_type).replace(' ', '_')}.nrrd"
+    img = sitk.Image([256, 128, 64], pixel_type)
+
+    fn = Path(tmp_path_factory.mktemp("data")) / fn
+    sitk.WriteImage(img, fn)
+    return fn
+
+
+_params = product(
+    [sitk.sitkInt8, sitk.sitkUInt8, sitk.sitkInt16, sitk.sitkUInt16, sitk.sitkFloat32],
+    ["nrrd", "nii"],
+)
+_params = chain(
+    _params,
+    product(
+        [
+            sitk.sitkInt8,
+            sitk.sitkUInt8,
+            sitk.sitkInt16,
+            sitk.sitkInt16,
+            sitk.sitkInt32,
+            sitk.sitkUInt32,
+        ],
+        ["dcm"],
+    ),
+)
+
+
+@pytest.fixture(scope="session", params=_params)
+def image_mha_2d(request, tmp_path_factory) -> Path:
+    pixel_type, extension = request.param
+    fn = f"image_2d_{sitk.GetPixelIDValueAsString(pixel_type).replace(' ', '_')}.nrrd"
+    img = sitk.Image([512, 256], pixel_type)
+
+    fn = Path(tmp_path_factory.mktemp("data")) / fn
+    sitk.WriteImage(img, fn)
+    return fn

test/test_dask.py
@@ -1,0 +1,59 @@
+import pytest
+import SimpleITK as sitk
+from SimpleITK.utilities.dask import from_sitk
+import numpy as np
+
+
+def test_from_sitk(image_mha):
+    sitk_img = sitk.ReadImage(image_mha)
+
+    img = from_sitk(image_mha)
+    img.compute()
+    assert img.shape == sitk_img.GetSize()[::-1]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_mha, chunks=(1, -1, -1))
+    print(f"chunks: {[c[0] for c in img.chunks]}")
+    img.compute()
+    assert [c[0] for c in img.chunks] == [
+        1,
+        sitk_img.GetSize()[1],
+        sitk_img.GetSize()[0],
+    ]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_mha, chunks=(1, 128, 128))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [1, 128, 128]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_mha, chunks=(-1, 83, 89))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [sitk_img.GetSize()[2], 83, 89]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+
+def test_from_sitk_2d(image_mha_2d):
+    sitk_img = sitk.ReadImage(image_mha_2d)
+
+    img = from_sitk(image_mha_2d)
+    img.compute()
+    assert img.shape == sitk_img.GetSize()[::-1]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_mha_2d, chunks=(128, -1))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [128, sitk_img.GetSize()[0]]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_mha_2d, chunks=(128, 128))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [128, 128]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)

blowekamp/SimpleITKExtras

test/conftest.py
@@ -1,0 +1,69 @@
+import pytest
+import SimpleITK as sitk
+from pathlib import Path
+from itertools import product, chain
+
+_params = product(
+    [sitk.sitkInt8, sitk.sitkUInt8, sitk.sitkInt16, sitk.sitkUInt16, sitk.sitkFloat32],
+    ["nrrd", "nii", "mha"],
+)
+
+_params = chain(
+    _params,
+    product(
+        [
+            sitk.sitkInt8,
+            sitk.sitkInt16,
+            sitk.sitkInt16,
+            sitk.sitkInt32,
+            sitk.sitkFloat32,
+        ],
+        ["mrc"],
+    ),
+)
+
+
+def to_ids(p):
+    return f"{p[1]}_{sitk.GetPixelIDValueAsString([0])}"
+
+
+@pytest.fixture(scope="session", params=_params)
+def image_fixture(request, tmp_path_factory) -> Path:
+    pixel_type, extension = request.param
+    fn = f"image_{sitk.GetPixelIDValueAsString(pixel_type).replace(' ', '_')}.nrrd"
+    img = sitk.Image([256, 128, 64], pixel_type)
+
+    fn = Path(tmp_path_factory.mktemp("data")) / fn
+    sitk.WriteImage(img, fn)
+    return fn
+
+
+_params = product(
+    [sitk.sitkInt8, sitk.sitkUInt8, sitk.sitkInt16, sitk.sitkUInt16, sitk.sitkFloat32],
+    ["nrrd", "nii", "mha"],
+)
+_params = chain(
+    _params,
+    product(
+        [
+            sitk.sitkInt8,
+            sitk.sitkUInt8,
+            sitk.sitkInt16,
+            sitk.sitkInt16,
+            sitk.sitkInt32,
+            sitk.sitkUInt32,
+        ],
+        ["dcm"],
+    ),
+)
+
+
+@pytest.fixture(scope="session", params=_params)
+def image_fixture_2d(request, tmp_path_factory) -> Path:
+    pixel_type, extension = request.param
+    fn = f"image_2d_{sitk.GetPixelIDValueAsString(pixel_type).replace(' ', '_')}.nrrd"
+    img = sitk.Image([512, 256], pixel_type)
+
+    fn = Path(tmp_path_factory.mktemp("data")) / fn
+    sitk.WriteImage(img, fn)
+    return fn

test/test_dask.py
@@ -1,0 +1,59 @@
+import pytest
+import SimpleITK as sitk
+from SimpleITK.utilities.dask import from_sitk
+import numpy as np
+
+
+def test_from_sitk(image_fixture):
+    sitk_img = sitk.ReadImage(image_fixture)
+
+    img = from_sitk(image_fixture)
+    img.compute()
+    assert img.shape == sitk_img.GetSize()[::-1]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_fixture, chunks=(1, -1, -1))
+    print(f"chunks: {[c[0] for c in img.chunks]}")
+    img.compute()
+    assert [c[0] for c in img.chunks] == [
+        1,
+        sitk_img.GetSize()[1],
+        sitk_img.GetSize()[0],
+    ]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_fixture, chunks=(1, 128, 128))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [1, 128, 128]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_fixture, chunks=(-1, 83, 89))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [sitk_img.GetSize()[2], 83, 89]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+
+def test_from_sitk_2d(image_fixture_2d):
+    sitk_img = sitk.ReadImage(image_fixture_2d)
+
+    img = from_sitk(image_fixture_2d)
+    img.compute()
+    assert img.shape == sitk_img.GetSize()[::-1]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_fixture_2d, chunks=(128, -1))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [128, sitk_img.GetSize()[0]]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)
+
+    img = from_sitk(image_fixture_2d, chunks=(128, 128))
+    img.compute()
+    assert [c[0] for c in img.chunks] == [128, 128]
+    assert np.array_equal(np.asarray(img), sitk.GetArrayViewFromImage(sitk_img))
+    assert img.dtype == sitk.extra._get_numpy_dtype(sitk_img)